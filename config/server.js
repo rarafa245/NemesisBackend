7c1234cee455b07ae50cd892f0dd33a7e59b0c87
Rafael <rarafa245@gmail.com>
2020-09-01 10:30:40 -0300

    First Structure for Server js

---
+++ config/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const multer = require( 'multer')
+const cors = require('cors')
+
+const server = express()
+const upload = multer()
+
+// Middlewares
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(upload.array())
+server.use(cors())
+
+
+module.exports = server
---
+++ run.js
@@ -0,0 +1,6 @@
+ 
+const server = require('./config')
+
+server.listen(5000, () => {
+    console.log('Server Start | Port 5000')
+})
